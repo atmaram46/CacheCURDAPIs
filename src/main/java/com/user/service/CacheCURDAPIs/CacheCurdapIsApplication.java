cd6ef171e2b2643de8f85a6bb66b051cb5dba432
Arun Kumar <arun.k.datika46@gmail.com>
2022-06-20 15:21:24 +0530

    New Changes

---
+++ src/main/java/com/user/service/CacheCURDAPIs/CacheCurdapIsApplication.java
@@ -0,0 +1,13 @@
+package com.user.service.CacheCURDAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheCurdapIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheCurdapIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/service/CacheCURDAPIs/CacheCurdapIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.service.CacheCURDAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheCurdapIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
